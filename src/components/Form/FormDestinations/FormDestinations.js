f352c7cd37ce67d3c1d04cca5fa247d4e343916d
Matheus Martins <shutt.oficial@gmail.com>
2022-10-30 13:27:56 -0300

    fazendo conexão com api para renderizar options de paises e cidades

--- src/components/Form/FormDestinations/FormDestinations.js
+++ src/components/Form/FormDestinations/FormDestinations.js
@@ -1,18 +1,62 @@
 import styles from "./FormDestinations.module.css";
+import { useState } from "react";
 
 function FormDestinations() {
+
+  const [countries, setCountries] = useState([]);
+  const [cities, setCities] = useState([]);
+
+  fetch("https://amazon-api.sellead.com/country", {
+    method: "GET",
+    headers: {
+      "Contente-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      setCountries(data);
+    });
+
+  fetch("https://amazon-api.sellead.com/city", {
+    method: "GET",
+    headers: {
+      "Contente-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      setCities(data);
+      console.log(data);;
+    });
+
   return (
     <fieldset className={styles.fieldContainer}>
       <legend>Destinos de Interesse:</legend>
 
-        <select className={styles.selectContainer} name="countries">
-          <option disabled selected>Selecione os países desejados</option>
-        </select>
-      
-        <select className={styles.selectContainer} name="cities">
-          <option disabled selected>Selecione as cidades desejadas</option>
-        </select>
+      <select className={styles.selectContainer} name="countries">
+        <option disabled selected>
+          Selecione os países desejados
+        </option>
+        {countries.map((country) => (
+          <option id={country.code.toString()} key={country.code.toString()}>
+            {country.name_ptbr}
+          </option>
+        ))}
+      </select>
 
+      <select className={styles.selectContainer} name="cities">
+        <option disabled selected>
+          Selecione as cidades desejadas
+        </option>
+        {cities.map(
+          (city) =>
+            (city.name_ptbr) && (
+              <option id={city.id} key={city.id}>
+                {city.name_ptbr}
+              </option>
+            )
+        )}
+      </select>
     </fieldset>
   );
 }
