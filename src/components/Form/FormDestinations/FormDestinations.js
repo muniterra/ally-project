eceedb0d23d9206d91190e0f12c2a79de106065a
Matheus Martins <shutt.oficial@gmail.com>
2022-10-30 16:26:33 -0300

    correção do erro ERR_INSUFFICIENT_RESOURCES

--- src/components/Form/FormDestinations/FormDestinations.js
+++ src/components/Form/FormDestinations/FormDestinations.js
@@ -1,39 +1,47 @@
 import styles from "./FormDestinations.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function FormDestinations() {
-
   const [countries, setCountries] = useState([]);
   const [cities, setCities] = useState([]);
 
-  fetch("https://amazon-api.sellead.com/country", {
-    method: "GET",
-    headers: {
-      "Contente-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      setCountries(data);
-    });
+  useEffect(() => {
+    fetch("https://amazon-api.sellead.com/country", {
+      method: "GET",
+      headers: {
+        "Contente-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setCountries(data);
+      });
+  }, []);
 
-  fetch("https://amazon-api.sellead.com/city", {
-    method: "GET",
-    headers: {
-      "Contente-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      setCities(data);
-    });
-
+  useEffect(() => {
+    fetch("https://amazon-api.sellead.com/city", {
+      method: "GET",
+      headers: {
+        "Contente-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setCities(data);
+      });
+  }, []);
+  
   return (
     <fieldset className={styles.fieldContainer}>
       <legend>Destinos de Interesse:</legend>
 
-      <select className={styles.selectContainer} multiple name="countries" required>
-        <option disabled defaultValue=''>
+      <select
+        className={styles.selectContainer}
+        multiple
+        name="countries"
+        required
+      >
+        <option disabled defaultValue="">
           Selecione os países desejados
         </option>
         {countries.map((country) => (
@@ -43,13 +51,18 @@ function FormDestinations() {
         ))}
       </select>
 
-      <select className={styles.selectContainer} multiple name="cities" required>
-        <option disabled defaultValue=''>
+      <select
+        className={styles.selectContainer}
+        multiple
+        name="cities"
+        required
+      >
+        <option disabled defaultValue="">
           Selecione as cidades desejadas
         </option>
         {cities.map(
           (city) =>
-            (city.name_ptbr) && (
+            city.name_ptbr && (
               <option id={city.id} key={city.id}>
                 {city.name_ptbr}
               </option>
